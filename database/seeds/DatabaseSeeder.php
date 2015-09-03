7fa9ff2cb300ddc2429675ae8c08a2eea2059968
William Tran <william@aligent.com.au>
2015-09-03 22:51:11 +0930

    Seeded users table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use \App\User;
 
 class DatabaseSeeder extends Seeder {
 
@@ -13,8 +14,21 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Model::unguard();
-
-		// $this->call('UserTableSeeder');
+	  	$this->call('UserTableSeeder');
 	}
+
+}
+
+class UserTableSeeder extends Seeder {
+
+  public function run()
+  {
+	DB::table('users')->delete();
+	User::create([
+	  'name'  => 'William Tran',
+	  'email' => 'will@wtran.net',
+	  'password'  => Hash::make('p@ssw0rd')
+	]);
+  }
 
 }
